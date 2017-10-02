dc71f3b5813a3d40c59892029a0e64c4f8ed8b85
richhcirerror <rich.hcir.error@gmail.com>
2017-10-03 01:13:55 +0400

    eq

--- src/main/java/com/neznayka/www/dao/config/ConfigDictionaryDAOImpl.java
+++ src/main/java/com/neznayka/www/dao/config/ConfigDictionaryDAOImpl.java
@@ -142,7 +142,7 @@ public class ConfigDictionaryDAOImpl implements ConfigDictionaryDAOIntf {
             keyAll=keyAll+key+" ";
         }
         // дополнительное условие вся фраза как тег
-        or.add(Restrictions.like("tagsJoin.tag", keyAll.trim(),MatchMode.EXACT));
+        or.add(Restrictions.eq("tagsJoin.tag", keyAll.trim()));
         query.add(or);
 
         answers.addAll(query.list());
