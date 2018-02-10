0840be33685577168db1d2b67fafb1390175192c
richhcirerror <rich.hcir.error@gmail.com>
2018-02-10 14:30:29 +0400

    matchmode AnyWhere

--- src/main/java/com/neznayka/www/dao/config/ConfigDictionaryDAOImpl.java
+++ src/main/java/com/neznayka/www/dao/config/ConfigDictionaryDAOImpl.java
@@ -136,7 +136,7 @@ public class ConfigDictionaryDAOImpl implements ConfigDictionaryDAOIntf {
             //сравниваем каждый тег
             for (String key : keyWords) {
                 if (key.length() > 4) {
-                    or.add(Restrictions.like("tagsJoin.tag", "%"+key+"%"));
+                    or.add(Restrictions.like("tagsJoin.tag", key, MatchMode.ANYWHERE));
                 } else {
                     or.add(Restrictions.eq("tagsJoin.tag", key));
                 }
